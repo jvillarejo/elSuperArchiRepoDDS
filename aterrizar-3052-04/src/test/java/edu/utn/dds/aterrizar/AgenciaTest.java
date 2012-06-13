9ffa942a818b94f29dc401e30e5c8a0ad41ee775
Federico Aloi <federico.aloi@gmail.com>
2012-06-13 04:30:47 -0300

    Agregados tests para la Agencia, see #7

---
+++ aterrizar-3052-04/src/test/java/edu/utn/dds/aterrizar/AgenciaTest.java
@@ -0,0 +1,63 @@
+package edu.utn.dds.aterrizar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.utn.dds.aterrizar.aerolineas.Aerolinea;
+import edu.utn.dds.aterrizar.agencia.Agencia;
+import edu.utn.dds.aterrizar.usuario.ConsultaAsientos;
+import edu.utn.dds.aterrizar.usuario.Usuario;
+import edu.utn.dds.aterrizar.vuelo.Asiento;
+import edu.utn.dds.aterrizar.vuelo.Vuelo;
+import edu.utn.dds.aterrizar.vuelo.filtros.FiltroAsiento;
+
+import static org.mockito.Mockito.*;
+
+
+public class AgenciaTest {
+
+	private Agencia laAgencia;
+	private List<Aerolinea> aerolineas;
+	private Aerolinea aerolineaMockito;
+	private Aerolinea aerolineaPrivadaDelTurco;
+
+	@Before
+	public void setUp() {
+		aerolineaMockito = mock(Aerolinea.class);
+		aerolineaPrivadaDelTurco = mock(Aerolinea.class);
+		aerolineas = new ArrayList<Aerolinea>();
+		aerolineas.add(aerolineaMockito);
+		aerolineas.add(aerolineaPrivadaDelTurco);
+		
+		laAgencia = new Agencia(aerolineas);
+	}
+	
+	@Test
+	public void laAgenciaBuscaAsientosEnTodasLasAerolineas() {
+		laAgencia.buscarAsientos(mock(Vuelo.class), mock(Usuario.class), mock(FiltroAsiento.class));
+		
+		verify(aerolineaMockito).buscarAsientos(any(Vuelo.class));
+		verify(aerolineaPrivadaDelTurco).buscarAsientos(any(Vuelo.class));
+	}
+	
+	@Test
+	public void laAgenciaRegistraLaConsultaEnElUsuario() {
+		Usuario juanMockito = mock(Usuario.class);
+		laAgencia.buscarAsientos(mock(Vuelo.class), juanMockito, mock(FiltroAsiento.class));
+
+		verify(juanMockito).registrarConsulta(any(ConsultaAsientos.class));
+	}
+	
+	@Test
+	public void laAgenciaFiltraLosAsientosSegunLosCriterios() {
+		FiltroAsiento filtros = mock(FiltroAsiento.class);
+		laAgencia.buscarAsientos(mock(Vuelo.class), mock(Usuario.class), filtros);
+
+		verify(filtros).filtrar(anyListOf(Asiento.class));
+	}
+
+	
+}
