faa0e32f1fe2db84bc3951bf2101af8139567ea6
ClaraAllende <clari.allende@gmail.com>
2012-10-09 01:57:05 -0300

    Cambié la dependencia de junit en el pom (estaba usando 3.8 en vez de 4.10 xD) y agregué los botoncitos de la grilla de resultados. Hay que armar una home para terminar de setear los bindings

--- aterrizar-3052-04-UI/src/main/java/edu/utn/dds/aterrizar/ui/busqueda/BusquedaVuelosWindow.java
+++ aterrizar-3052-04-UI/src/main/java/edu/utn/dds/aterrizar/ui/busqueda/BusquedaVuelosWindow.java
@@ -6,7 +6,11 @@ import edu.utn.dds.aterrizar.ui.transformers.*;
 import edu.utn.dds.aterrizar.vuelo.Asiento;
 import edu.utn.dds.aterrizar.vuelo.Busqueda;
 
+import org.uqbar.arena.actions.MessageSend;
+import org.uqbar.arena.bindings.NotNullObservable;
 import org.uqbar.arena.layout.ColumnLayout;
+import org.uqbar.arena.layout.HorizontalLayout;
+import org.uqbar.arena.widgets.Button;
 import org.uqbar.arena.widgets.Label;
 import org.uqbar.arena.widgets.Panel;
 import org.uqbar.arena.widgets.TextBox;
@@ -23,12 +27,17 @@ public class BusquedaVuelosWindow extends SimpleWindow<Busqueda>{
 	}
 
 	private static final long serialVersionUID = -7226693291529135759L;
-
+	
 	@Override
-	protected void addActions(Panel arg0) {
-		// TODO Auto-generated method stub
+	protected void addActions(Panel actionsPanel) {
+		new Button(actionsPanel)
+		.setCaption("Buscar")
+		.onClick(new MessageSend(this.getModelObject(), "search"))
+		.setAsDefault()
+		.disableOnError();
 		
 	}
+	
 	
 	@Override
 	protected void createMainTemplate(Panel mainPanel) {
@@ -38,6 +47,7 @@ public class BusquedaVuelosWindow extends SimpleWindow<Busqueda>{
 		super.createMainTemplate(mainPanel);
 
 		this.createResultsGrid(mainPanel);
+		this. createGridActions(mainPanel);
 	}
 
 	// *************************************************************************
@@ -59,8 +69,31 @@ public class BusquedaVuelosWindow extends SimpleWindow<Busqueda>{
 
 		this.describeResultsGrid(table);
 	}
+	protected void createGridActions(Panel mainPanel) {
+		Panel actionsPanel = new Panel(mainPanel);
+		actionsPanel.setLayout(new HorizontalLayout());
+		
+		Button buy = new Button(actionsPanel)
+		.setCaption("Comprar")
+		.onClick(new MessageSend(this, "comprarAsiento"));
+		
+		Button makeReservation = new Button(actionsPanel)
+		.setCaption("Reservar")
+		.onClick(new MessageSend(this,"reservarAsiento"));
+		
+		new Button(actionsPanel)
+		.setCaption("Cerrar")
+		.onClick(new MessageSend(this, "cerrar"));
+		
+		// Deshabilitar los botones si no hay ningún elemento seleccionado en la grilla.
+//		NotNullObservable elementSelected = new NotNullObservable("vueloSeleccionado");
+//		buy.bindEnabled(elementSelected);
+//		makeReservation.bindEnabled(elementSelected);
+	}
 
 	protected void describeResultsGrid(Table<Asiento> table) {
+		//this.createSimpleColumn(table, "Aerolinea", "aerolinea");
+		//this.createSimpleColumn(table, "Vuelo", "codigo");
 		this.createSimpleColumn(table, "Asiento", "numeroDeAsiento");
 		this.createSimpleColumn(table, "Precio", "precio");
 		
@@ -88,4 +121,14 @@ public class BusquedaVuelosWindow extends SimpleWindow<Busqueda>{
 		new TextBox(searchFormPanel).bindValueToProperty("fechaSalida");
 	}
 
+	public void comprarAsiento(){
+		//TODO: acá deberíamos llamar al comprar de la agencia. Eso podría estar en una home
+	}
+	public void reservarAsiento(){
+		//TODO: idem lo de arriba
+	}
+	
+	public void cerrar(){
+		this.close();
+	}
 }
